e0fc0b1276e4466a37e958f38ad917105888dd1e
johannes.guettler <johannes.guettler@gmail.com>
2012-04-19 23:58:33 +0000

    klassendesign umgeworfen, +ICalendar.java, google-calendar einträge jetzt über GoogleConnect.createGoogleCalendarEntry

--- Abrechnungsprogramm/Abrechnungsprogramm/src/de/drk/akrd/DRManager.java
+++ Abrechnungsprogramm/Abrechnungsprogramm/src/de/drk/akrd/DRManager.java
@@ -47,47 +47,6 @@ public class DRManager {
     return INSTANCE;
   }
 
-  /**
-   * creates Googleclaendar-entrys for the saved shifts
-   * @return true if successful, false otherwise
-   */
-  public boolean createGoogleCalendarEntry() {
-    if (savedShifts == null || savedShiftDates == null) {
-      return false;
-    }
-    Calendar cal = Calendar.getInstance();
-    String[][] entryStrings = new String[savedShifts.length][4];
-      SimpleTimeZone mez = new SimpleTimeZone(+1 * 60 * 60 * 1000, "ECT");
-      mez.setStartRule(Calendar.MARCH, -1, Calendar.SUNDAY, 2 * 60 * 60 * 1000);
-      mez.setEndRule(Calendar.OCTOBER, -1, Calendar.SUNDAY, 2 * 60 * 60 * 1000);
-      for (int i = 0; i < savedShifts.length; i++) {
-        Shift shift = savedShifts[i];
-        int startHour = (int) (shift.getStartingTime() / 100);
-        int startMinutes = (int) (shift.getStartingTime() % 100);
-        int endHour = (int) (shift.getEndTime() / 100);
-        int endMinutes = (int) (shift.getEndTime() % 100);
-        cal.setTime(savedShiftDates[i]);
-        int day = cal.get(Calendar.DAY_OF_MONTH);
-        int month = cal.get(Calendar.MONTH);
-        int year = cal.get(Calendar.YEAR);
-        int timeCorrection = (mez.inDaylightTime(cal.getTime()))? 2: 1;
-        String beginString = getDateTimeString(year, month, day, startHour, startMinutes, timeCorrection);
-        if(endHour<startHour) {
-          // night shift
-          cal.add(Calendar.DATE, 1);
-          day = cal.get(Calendar.DAY_OF_MONTH);
-          month = cal.get(Calendar.MONTH);
-          year = cal.get(Calendar.YEAR);
-        }
-        String endString = getDateTimeString(year, month, day, endHour, endMinutes, timeCorrection);
-        entryStrings[i][0] = savedShifts[i].getId().substring(0, 3);
-        entryStrings[i][1] = "Termintext";
-        entryStrings[i][2] = beginString;
-        entryStrings[i][3] = endString;
-      }
-      GoogleConnect.createNewAppointment(entryStrings);
-    return false;
-  }
   public void parseDutyRota() {
     String filePath = getPdfFilePath();
     String[] contentStrings = parsePdf(filePath);
@@ -151,9 +110,6 @@ public class DRManager {
     }
   }
 
-private static String getTwoLetterStringFromInt(int x) {
-  return ((x<10)? ("0"+x): x+"");
-}
 private static String getPdfFilePath() {
   JFileChooser fileChooser = new JFileChooser(new File(System.getProperty("user.dir")));
   fileChooser.setFileFilter(new FileFilter() {
@@ -174,15 +130,6 @@ private static String getPdfFilePath() {
     return fileChooser.getSelectedFile().getPath();
   }
   return null;
-}
-private static String getDateTimeString(int year, int month, int day, int hour, int minute, int timeCorrection) {
-  String monthString = getTwoLetterStringFromInt(month + 1);
-  String dayString = getTwoLetterStringFromInt(day);
-  String returnString = year + "-" + monthString + "-" + dayString + "T"
-          + getTwoLetterStringFromInt(hour) + ":"
-          + getTwoLetterStringFromInt(minute) + ":" + "00+0"
-          + timeCorrection + ":00";
-  return returnString;
 }
   /**
    * 
